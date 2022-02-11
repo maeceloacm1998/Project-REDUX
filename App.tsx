5467edc72fa8efaadd8215046731d4404687ceba
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-11 13:31:05 -0300

    chore: init project and basic config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Navigation from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <StatusBar
+        translucent={true}
+        barStyle="dark-content"
+        backgroundColor="#FFF"
+      />
+      <Navigation />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
