ef7682072b6e6072df495c2cf2ac26fb5fb675c3
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-12 14:30:16 -0300

    feat: createTicketScreen in progress

--- src/screens/CreateTicketScreen.tsx
+++ src/screens/CreateTicketScreen.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import { useForm } from "react-hook-form";
+import InputCreateTicketScreen from "../components/InputCreateTicketScreen/InputCreateTicketScreen";
+
 export default function CreateTicketScreen() {
+  const { control, handleSubmit } = useForm();
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
     },
   });
 
   return (
     <View style={styles.container}>
+      <InputCreateTicketScreen control={control} name="Teste" />
       <Text>Essa daqui ea tela de criar o ticket</Text>
     </View>
   );
