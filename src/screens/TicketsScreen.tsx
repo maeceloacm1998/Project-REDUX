ebb0caa909d8743a6c246909471b91b888f6101f
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-14 21:24:26 -0300

    refactor: include notFound style and loading when remove ticket

--- src/screens/TicketsScreen.tsx
+++ src/screens/TicketsScreen.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  Dimensions,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 
 import HeaderTicketList from "../components/HeaderTicketsLIst/HeaderTicketList";
@@ -11,6 +18,8 @@ import { ApplicationReducer, getListTickets } from "../redux";
 import { Alert } from "react-native";
 
 export default function TicketsScreen({ navigation }: ScreenProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const dispatch = useDispatch();
   const createTicket = () => navigation.navigate("CriarTicket");
   const pressTicketCard = () => console.log("press card");
@@ -36,8 +45,10 @@ export default function TicketsScreen({ navigation }: ScreenProps) {
   };
 
   const removeTicket = (ticketID: string) => {
+    setLoading(true);
     const newListTicket = listTickets.filter((item) => item.id !== ticketID);
     dispatch(getListTickets(newListTicket));
+    setLoading(false);
   };
 
   const styles = StyleSheet.create({
@@ -45,24 +56,45 @@ export default function TicketsScreen({ navigation }: ScreenProps) {
       flex: 1,
       backgroundColor: colors.background,
     },
+    containerSpinner: {
+      height: Dimensions.get("window").height - 90,
+      justifyContent: "center",
+      alignItems: "center",
+    },
     containerTicketsList: {
       flex: 1,
       paddingHorizontal: 20,
     },
+    textNotFound: {
+      fontSize: 17,
+      fontWeight: "bold",
+      color: colors.primary,
+    },
   });
-
   return (
     <View style={styles.container}>
       <HeaderTicketList onPressButton={createTicket} />
-      <ScrollView style={styles.containerTicketsList}>
-        {listTickets.map((item) => (
-          <TicketCard
-            item={item}
-            onPressCard={pressTicketCard}
-            onPressRemove={alertRemoveTicket}
-          />
-        ))}
-      </ScrollView>
+      {loading && (
+        <View style={styles.containerSpinner}>
+          <ActivityIndicator size={25} color={colors.primary} />
+        </View>
+      )}
+      {!listTickets.length && (
+        <View style={styles.containerSpinner}>
+          <Text style={styles.textNotFound}>Sem Tickets cadastrados</Text>
+        </View>
+      )}
+      {!loading && (
+        <ScrollView style={styles.containerTicketsList}>
+          {listTickets.map((item) => (
+            <TicketCard
+              item={item}
+              onPressCard={pressTicketCard}
+              onPressRemove={alertRemoveTicket}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
